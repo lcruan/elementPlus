36f138e64abe1c8291353a4447dc55d83b476554
lcruan <1416802331@qq.com>
2024-04-13 10:42:32 +0800

    熟悉按钮

---
+++ learn-elementPlus/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const tabPosition = ref('left')
+
+import elButton from '@/views/elButton.vue'
+</script>
+
+<template>
+  <el-tabs :tab-position="tabPosition" style="height: 100%" class="demo-tabs">
+    <el-tab-pane label="按钮">
+      <elButton></elButton>
+    </el-tab-pane>
+    <el-tab-pane label="Config">Config</el-tab-pane>
+    <el-tab-pane label="Role">Role</el-tab-pane>
+    <el-tab-pane label="Task">Task</el-tab-pane>
+  </el-tabs>
+</template>
+
+<style scoped></style>
